2bd84418dc82ce78d0a5adab1762170c25a62347
sujayneglur123 <sujayneglur123@gmail.com>
2018-08-31 16:03:16 -0400

    Initial Commit

---
+++ server.js
@@ -0,0 +1,49 @@
+const express= require('express');
+const hbs= require('hbs');
+const fs= require('fs');
+var app = express();
+app.set('view engine','hbs');
+hbs.registerPartials(__dirname+'/views/partials');
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+hbs.registerHelper('screamIt', (text) => text.toUpperCase());
+
+app.use((request,response,next) => {
+    var now =new Date().toString();
+    var log =`${now} : ${request.method} ${request.url}`;
+    fs.appendFile('server.log',log+'\n', (error) => {
+        if(error){
+            console.log(error);
+        }
+    });
+    next();
+});
+
+// app.use((request,response,next) => {
+//     response.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname+'/public'));
+
+app.get('/', (request,response) => {
+    // response.send('<h1>Hello express JS</h1>');
+    response.render('home.hbs',{
+        pageTitle:'Home Page',
+        welcomeMessage:'This is my website'
+    })
+});
+
+app.get('/about',(request,response) => {
+    response.render('about.hbs', {
+        pageTitle:'About page'
+    });
+});
+
+app.get('/bad',(request,response) => {
+    response.send({
+        errorMessage:'Error in handling the request'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
